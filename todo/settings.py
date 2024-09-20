6c1906dcc6fa13d24b965ba07c3e8a0ac8f1cb2f
Pandiri Pranav <pskoti4567@gmail.com>
2024-09-21 01:30:41 +0530

    updated

--- todo/settings.py
+++ todo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'practise_app1',
+    'todolist_app1',
 ]
 
 MIDDLEWARE = [
